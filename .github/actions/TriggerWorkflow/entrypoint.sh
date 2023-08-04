bd0ad43413c8fc3f4afefab53c17a83c04086420
Ahmed Hassan <a_h_mahmoud@hotmail.com>
2023-08-04 02:19:37 -0400

    Updates

--- .github/actions/TriggerWorkflow/entrypoint.sh
+++ .github/actions/TriggerWorkflow/entrypoint.sh
@@ -1,20 +1,47 @@
 #!/bin/sh
 
-if [ -z "$webhook_url" ]
+if [ -z "$destination_repo" ]
 then
-  echo "Webhook url can not be empty"
+  echo "Destination repository can not be empty"
   return 1
 fi
 
-if [ -z "$webhook_secret" ]
+if [ -z "$destination_branch" ]
 then
-  echo "Webhook secret can not be empty"
+  destination_branch="workflows/$GITHUB_SHA/$GITHUB_RUN_ID"
+fi
+
+if [ -z "$workflow_file" ]
+then
+   echo "Workflow file can not be empty"
   return 1
+fi
+
+if [ -z "$api_token" ]
+then
+  api_token="$API_TOKEN_GITHUB"
 fi
 
 curl \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $webhook_secret"\
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  $webhook_url
+-X POST \
+-H "Accept: application/vnd.github.v3+json" \
+-H "Authorization: token $api_token" \
+"https://api.github.com/repos/$destination_repo/actions/workflows/$workflow_file/dispatches" \
+-d '{"ref":"$destination_branch"}'
+
+sleep 10
+
+ workflow_run_id=$(curl -s -H "Authorization: token $api_token" \
+  "https://api.github.com/repos/$destination_repo/actions/runs?branch=$destination_branch"  | jq -r '.workflow_runs[0].id')
+
+# workflow_run_id=$(workflow_last_run_id)
+# while [ $workflow_run_id -eq $workflow_last_run_id ]
+# do
+ 
+#   echo "Waiting workflow ..."
+ 
+#   counter=$((counter + 1))  
+# done
+
+workflow_run_url="https://github.com/ahmedhassan16/artifacts/actions/runs/$workflow_run_id"
+echo "URL of the most recent workflow run: $workflow_run_url"
